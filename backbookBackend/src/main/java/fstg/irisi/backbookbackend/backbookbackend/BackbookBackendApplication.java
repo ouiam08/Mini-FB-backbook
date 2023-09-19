1860a142dd818e5cc5f0441dd23af8d3c9e94c74
ouiam08 <ouiam.khoudraji@gmail.com>
2023-09-19 13:12:11 +0100

    initiation

---
+++ backbookBackend/src/main/java/fstg/irisi/backbookbackend/backbookbackend/BackbookBackendApplication.java
@@ -0,0 +1,13 @@
+package fstg.irisi.backbookbackend.backbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackbookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackbookBackendApplication.class, args);
+    }
+
+}
