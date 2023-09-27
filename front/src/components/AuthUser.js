9d40378b1039dc719c36e9fc56faed3fc6d1e84d
amina <aminaelansari21@gmail.com>
2023-09-27 08:33:44 +0100

    sign in design

--- front/src/components/AuthUser.js
+++ front/src/components/AuthUser.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Header from './Layouts/Header'
+import Footer from './Layouts/Footer'
+import Earth from './../Assets/images/earth.jpg'
 
 const AuthUser = ({formData, handleInputChange, handleSubmit, response}) => {
     return (
-        <div>
-            <h1>Authentication</h1>
+        <div className="bg-gray-200 h-screen">
+            <Header />
+            <div className="flex items-center justify-center h-full w-full">
+            <div className="inline-flex align-center bg-white shadow-md rounded-lg m-4 p-6 text-center">
+                <div className="">
+                    <img src={Earth} alt=""/>
+                </div>
+                <div className="">
+                    <h1 className="text-center m-4 text-2xl font-bold">Enjoy your time !!!</h1>
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="username">Username:</label>
@@ -13,6 +23,7 @@ const AuthUser = ({formData, handleInputChange, handleSubmit, response}) => {
                         name="name"
                         value={formData.name}
                         onChange={handleInputChange}
+                        className="bg-gray-200 rounded-lg m-6 p-1"
                     />
                 </div>
                 <div>
@@ -23,11 +34,17 @@ const AuthUser = ({formData, handleInputChange, handleSubmit, response}) => {
                         name="password"
                         value={formData.password}
                         onChange={handleInputChange}
+                        className="bg-gray-200 rounded-lg m-6 p-1"
                     />
                 </div>
-                <button type="submit">Sign In</button>
+                <button type="submit" className="bg-green-600 text-white rounded-lg p-2 m-2">Sign In</button>
                 <h3>{response}</h3>
             </form>
+                </div>
+                
+            </div>
+            </div>
+            <Footer />
         </div>
     );
 };
