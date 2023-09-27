0c9319534506b0ed6eddbd6bf7994227043acc23
Amina EL ANSARI <aminaelansari21@gmail.com>
2023-09-27 23:27:56 +0100

    header and footer refactoring

--- front/src/components/Layouts/Footer.js
+++ front/src/components/Layouts/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <>
-            <div className='bg-white text-gray-700 text-center shadow-md mt-[-4] fixed bottom-0 w-full p-2'>
+            <div className='bg-white text-gray-700 text-center shadow-md mt-[-4]  bottom-0 w-full p-2'>
                 Copy rights 2023@
             </div>
         </>
--- front/src/components/Layouts/Header.js
+++ front/src/components/Layouts/Header.js
@@ -12,15 +12,18 @@ function Header(
         <>
             <header className="bg-white text-black-900 p-4 text-center shadow-md p-4 flex items-center justify-between">
                 <a href='/'><img src={BICON} alt='img' className='w-12 h-12 ml-3 cursor-pointer'/></a>
-                <a href='/profile'>
+                <div className='inline-flex'>
+                    <a href='/profile'>
                     <div className="inline-flex items-center cursor-pointer">
                         <img src={userImage} alt='user' className='w-10 h-10 rounded-full p-2'/>
                         <span>{userName}</span>
                     </div> 
                 </a>
-                {Cookies.get('userID') !== '0' && <div>
+                {Cookies.get('userID') !== '0' && <div className='ml-6'>
                     <button className='bg-green-600 text-white p-2 rounded-lg' onClick={props.handleDeconnexion}>Deconnexion</button>
                 </div>}
+                </div>
+                
             </header>
 
         </>
